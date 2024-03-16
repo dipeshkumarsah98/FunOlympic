6cf1636dc6683574f661f6f25c5f04d7e155ea1f
prabes <dprabesh11@gmail.com>
2024-03-17 00:43:37 +0545

    Individual sport page with dynamic routing-only UI

---
+++ funOlympic-client/src/app/(client)/sports/[id]/EventsTable.jsx
@@ -0,0 +1,7 @@
+// #TODO move this to component
+const EventsTable = () => {
+  return (
+<div className="ps-30 pt-80">Tabbed Table here: Event Schedule | Results</div>
+)}
+
+export default EventsTable;
--- funOlympic-client/src/app/(client)/sports/page.jsx
+++ funOlympic-client/src/app/(client)/sports/page.jsx
@@ -1,10 +1,10 @@
 export default function Sports() {
-  const products = [
+  const sports = [
     {
       id: 1,
       name: 'Football',
       href: '#',
-      price: 'Time: 6:00 (+5:45 GMT)',
+      time: 'Time: 6:00 (+5:45 GMT)',
       imageSrc: 'https://cdn.pixabay.com/photo/2022/03/08/15/44/boy-7056003_1280.jpg',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
@@ -12,7 +12,7 @@ export default function Sports() {
       id: 2,
       name: 'Nomad Tumbler',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
       imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
@@ -20,7 +20,7 @@ export default function Sports() {
       id: 3,
       name: 'Focus Paper Refill',
       href: '#',
-      price: '$89',
+      time: '$89',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
       imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
     },
@@ -28,66 +28,65 @@ export default function Sports() {
       id: 4,
       name: 'Machined Mechanical Pencil',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
 
 		{
-      id: 4,
+      id: 5,
       name: 'Machined Mechanical Pencil',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
 
     {
-      id: 4,
+      id: 6,
       name: 'Machined Mechanical Pencil',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
 
     {
-      id: 1,
+      id: 7,
       name: 'Football',
       href: '#',
-      price: 'Time: 6:00 (+5:45 GMT)',
+      time: 'Time: 6:00 (+5:45 GMT)',
       imageSrc: 'https://cdn.pixabay.com/photo/2022/03/08/15/44/boy-7056003_1280.jpg',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
 
     {
-      id: 4,
+      id: 8,
       name: 'Machined Mechanical Pencil',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
 
     {
-      id: 4,
+      id: 9,
       name: 'Machined Mechanical Pencil',
       href: '#',
-      price: '$35',
+      time: '$35',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
 
     {
-      id: 1,
+      id: 10,
       name: 'Football',
       href: '#',
-      price: 'Time: 6:00 (+5:45 GMT)',
+      time: 'Time: 6:00 (+5:45 GMT)',
       imageSrc: 'https://cdn.pixabay.com/photo/2022/03/08/15/44/boy-7056003_1280.jpg',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
   ]
-
 
   return (
     <>
@@ -95,17 +94,17 @@ export default function Sports() {
 		    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h1 className="mb-10 text-lg">All Sports</h1>
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {products.map((product) => (
-                <a key={product.id} href={product.href} className="group">
+              {sports.map((sport) => (
+                <a key={sport.id} href={`/sports/${sport.id}`} className="group">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={sport.imageSrc}
+                      alt={sport.imageAlt}
                       className="h-full w-full object-cover object-center group-hover:opacity-75"
                     />
                   </div>
-                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                  <h3 className="mt-4 text-sm text-gray-700">{sport.name}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">{sport.time}</p>
                 </a>
           ))}
           </div>
